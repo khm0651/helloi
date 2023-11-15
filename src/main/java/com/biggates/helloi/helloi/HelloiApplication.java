f9f6d8b2e61cbab4e265f668c5a8c5dae267d0dc
hare <khm0651@gmail.com>
2023-11-15 21:17:19 +0900

    first commit

---
+++ src/main/java/com/biggates/helloi/helloi/HelloiApplication.java
@@ -0,0 +1,13 @@
+package com.biggates.helloi.helloi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biggates/helloi/helloi/HelloiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biggates.helloi.helloi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
